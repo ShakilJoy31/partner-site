0c3537d9b6906c4e1d17b2f09b5ef7692fc7c5b0
ShakilJoy31 <joy31a@gmail.com>
2023-07-03 14:48:59 +0600

    animation is added

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,25 @@
 import "@/styles/globals.css";
 import PartnerHeading from "./Components/PartnerHeading";
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(()=>{
+    AOS.init({duration: 1000})
+  },[])
   return (
     <div className="bg-white">
-      <div>
+      <div data-aos='zoom-in'>
       <img className="w-full" src="https://i.ibb.co/sbRcV8b/Business.png" alt="" />
       </div>
 
       <div className="lg:flex md:flex lg:mx-12 md:mx-8">
-        <div>
+        <div data-aos='fade-right'>
           <PartnerHeading></PartnerHeading>
         </div>
 
-        <div className="mt-[48px] ml-[48px] w-full">
+        <div className="mt-[48px] ml-[48px] w-full" data-aos='fade-left'>
           <Component {...pageProps} />
         </div>
       </div>
